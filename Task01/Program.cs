9c2a879dfb24cd4a3d7f5faec5ce7500ad75dae4
Artem <lebro53@mail.ru>
2023-02-12 21:47:30 +0300

    Added solution task01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.
+﻿Console.Write("Введите первое число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите первое число: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+int sqrTwo = numberTwo * numberTwo;
+if (sqrTwo == numberOne)
+{
+	Console.WriteLine("Да, первое число является квадратом второго");
+}
+else
+{
+	Console.WriteLine("Нет, первое число не является квадратом второго");
+}
