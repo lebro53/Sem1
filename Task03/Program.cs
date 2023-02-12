2488a98049e33f78e0457f117f8b619804057bc8
Artem <lebro53@mail.ru>
2023-02-12 22:06:15 +0300

    added solution task03

---
+++ Task03/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 1) Console.WriteLine("Monday");
+if (number == 2) Console.WriteLine("Tuesday");
+if (number == 3) Console.WriteLine("Wednesday");
+if (number == 4) Console.WriteLine("Thursday");
+if (number == 5) Console.WriteLine("Friday");
+if (number == 6) Console.WriteLine("Saturday");
+if (number == 7) Console.WriteLine("Sunday");
+if (number > 7) Console.WriteLine("Error. Укажите число от 1 до 7!");
