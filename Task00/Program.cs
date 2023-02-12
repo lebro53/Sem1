cb843a8d0048b716beb8e68dbca57128871227b8
Artem <lebro53@mail.ru>
2023-02-12 21:27:54 +0300

    Added solution task00

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число для расчета его квадрата: ");
+double number = Convert.ToDouble(Console.ReadLine()); // Сделал вместо int через функцию double, т.к. расширяет наши способности
+double sqr = number * number;
+Console.WriteLine($"Квадрат числа {number} = {sqr}");
