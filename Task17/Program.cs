6ea36d92fb95f74ea21270edf0bb4bbb45fda24e
Artem <lebro53@mail.ru>
2023-02-19 00:08:07 +0300

    Решил задачу по своему

---
+++ Task17/Program.cs
@@ -0,0 +1,32 @@
+﻿// Methods-----------------------------------------
+int Promt(string massege)
+{
+	Console.Write(massege);
+	string value = Console.ReadLine();
+	int result = Convert.ToInt32(value);
+	return result;
+}
+int Quarter(int pointX, int pointY)
+{
+	if (pointX > 0 && pointY > 0) return 1;
+	if (pointX < 0 && pointY < 0) return 3;
+	if (pointX < 0 && pointY > 0) return 2;
+	if (pointX > 0 && pointY < 0) return 4;
+	else return 0;
+}
+bool ConditionCheck(int pointX, int pointY)
+{
+	if (pointX != 0 && pointY != 0)
+	{
+		return true;
+	}
+	Console.WriteLine("Координаты числа A не должны быть равны 0");
+	return false;
+}
+
+// Another code----------------------------------------------
+
+int xa = Promt("Введи координату х для точки А: ");
+int ya = Promt("Введи координату у для точки А: ");
+
+if (ConditionCheck(xa, ya)) Console.WriteLine($"Данные координаты соответствуют {Quarter(xa, ya)} четверти");
