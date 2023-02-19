64c26d3d7514c4ef963419becb4f6ecfe071cf79
Artem <lebro53@mail.ru>
2023-02-19 20:27:09 +0300

    Added solution Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,20 @@
+﻿// Methods-----------------------------------------
+int Promt(string massege)
+{
+	Console.Write(massege);
+	string value = Console.ReadLine();
+	int result = Convert.ToInt32(value);
+	return result;
+}
+void Square(int num)
+{
+	for (int i = 1; i <= num; i++)
+	{
+		Console.WriteLine($"{i,5} = {i * i * i,5}");
+	}
+}
+
+// Program body --------------------------------
+int number = Promt("Введите натуральное число: ");
+if (number > 0) Square(number);
+else Console.WriteLine("Ошибка. Введено не натуральное число");
