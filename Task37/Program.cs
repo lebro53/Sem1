92d6608a6ec224f18d466584d9f2e0ef0d5740bb
Artem <lebro53@mail.ru>
2023-02-27 22:14:12 +0300

    Added solution Task37

---
+++ Task37/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+// Method--------------------------------------------------
+int Promt(string massege)
+{
+	Console.WriteLine(massege);
+	string value = Console.ReadLine();
+	int result = Convert.ToInt32(value);
+	return result;
+
+}
+int[] CreateArray(int num, int min, int max)
+{
+	int[] array = new int[num];
+	Random rnd = new Random();
+	for (int i = 0; i < array.Length; i++)
+	{
+		array[i] = rnd.Next(min, max + 1);
+	}
+	return array;
+}
+void PrintArray(int[] array)
+{
+	Console.Write("[");
+	for (int i = 0; i < array.Length; i++)
+	{
+		if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+		else Console.WriteLine($"{array[i]}]");
+	}
+}
+int[] CreateNewArray(int num, int[] array)
+{
+	if (num % 2 == 0)
+	{
+		int[] newArray = new int[num / 2];
+		for (int i = 0; i < newArray.Length; i++)
+		{
+			newArray[i] = array[i] * array[array.Length - 1 - i];
+		}
+		return newArray;
+	}
+	else
+	{
+		int[] newArray = new int[num / 2 + 1];
+		for (int i = 0; i < newArray.Length; i++)
+		{
+			newArray[i] = array[i] * array[array.Length - 1 - i];
+		}
+		newArray[newArray.Length - 1] = array[array.Length / 2];
+		return newArray;
+	}
+}
+
+// Program body--------------------------------------------
+int size = Promt("Введите размер массива: ");
+int minNumberArray = Promt("Введите начало массива: ");
+int maxNumberArray = Promt("Введите конец массива: ");
+int[] arr = CreateArray(size, minNumberArray, maxNumberArray);
+Console.Clear();
+PrintArray(arr);
+PrintArray(CreateNewArray(size, arr));
