dff4db14d7a5735a085da4564bbf3d9d51b12852
Artem <lebro53@mail.ru>
2023-03-02 13:28:53 +0300

    Added solution task45

---
+++ Task45/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 45: Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного
+// копирования.
+
+// Method--------------------------------------------------
+int Promt(string massege)
+{
+	Console.WriteLine(massege);
+	string value = Console.ReadLine();
+	int result = Convert.ToInt32(value);
+	return result;
+
+}
+int[] CreateArray(int num, int min, int max)
+{
+	int[] array = new int[num];
+	Random rnd = new Random();
+	for (int i = 0; i < array.Length; i++)
+	{
+		array[i] = rnd.Next(min, max + 1);
+	}
+	return array;
+}
+int[] CopyArray(int[] array)
+{
+	int[] newArray = new int[array.Length];
+	for (int i = 0; i < array.Length; i++)
+	{
+		newArray[i] = array[i];
+	}
+	return newArray;
+}
+void PrintArray(int[] array)
+{
+	Console.Write("[");
+	for (int i = 0; i < array.Length; i++)
+	{
+		if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+		else Console.WriteLine($"{array[i]}]");
+	}
+}
+
+// Program body--------------------------------------------
+int size = Promt("Введите размер массива: ");
+int minNumberArray = Promt("Введите начало массива: ");
+int maxNumberArray = Promt("Введите конец массива: ");
+int[] arr = CreateArray(size, minNumberArray, maxNumberArray);
+Console.Clear();
+PrintArray(arr);
+int[] newArr = CopyArray(arr);
+PrintArray(newArr);
